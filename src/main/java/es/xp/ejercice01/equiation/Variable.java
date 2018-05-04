6456b35e0a6fe892e52f0a0b1de7174041e282fb
Abraham Santana <ezsanab@ericsson.com>
2018-05-04 19:50:24 +0200

    Variable class added

--- src/main/java/es/xp/ejercice01/equiation/Variable.java
+++ src/main/java/es/xp/ejercice01/equiation/Variable.java
@@ -1,5 +1,55 @@
 package es.xp.ejercice01.equiation;
 
-public class Variable {
+import java.util.Set;
+
+public class Variable extends Term{
+	private String name;
+
+	public Variable(float value, String name) {
+		super(value);
+		this.name = name;
+	}
+	
+	public String getName() {	
+		return this.name;
+	}
+	
+	@Override
+	public boolean hasName(String name) {
+		return this.name.equals(name);
+	}
+	
+	@Override
+	public boolean hasName(Set<String> names) {
+		return names.contains(this.name);
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if(!super.equals(obj))
+			return false;
+		Variable other = (Variable) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
+	@Override
+	protected Variable clone(){
+		return new Variable(this.getValue(),this.name);
+	}
+
+	@Override
+	public String toString() {
+		return "" + this.getValue() + this.name;
+	}
+
+	@Override
+	void dispatch(TermVisitor visitor) {
+		visitor.visit(this);
+	}
 
 }
--- src/test/java/es/xp/ejercice01/equiation/VariableTest.java
+++ src/test/java/es/xp/ejercice01/equiation/VariableTest.java
@@ -2,13 +2,78 @@ package es.xp.ejercice01.equiation;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Test;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class VariableTest {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
+	@ParameterizedTest
+	@ValueSource(strings = { "x", "y" })
+	void constructorTest(String name) {
+		Variable variable = new Variable(5, name);
+		assertEquals(5, variable.getValue());
+		assertEquals(name, variable.getName());
+		
+	}
+	@ParameterizedTest
+	@ValueSource(strings = { "x", "y" })
+	public void hasNameTest(String name) {
+		Variable variable = new Variable(5, name);
+		assertTrue(variable.hasName(name));
+	}	
+	
+	@ParameterizedTest
+	@ValueSource(strings = { "x", "y" })
+	public void notHasNameTest(String name) {
+		Variable variable = new Variable(5, name);
+		assertFalse(variable.hasName("z"));
+	}	
+	
+	@ParameterizedTest
+	@ValueSource(strings = { "x", "y" })
+	public void hasSetNameTest(String name) {
+		Variable variable = new Variable(5, name);
+		Set<String> names = new HashSet<String>();
+		names.add(name);
+		assertTrue(variable.hasName(names));
+	}
+	
+	@ParameterizedTest
+	@ValueSource(strings = { "x", "y" })
+	public void notHasSetNameTest(String name) {
+		Variable variable = new Variable(5, name);
+		Set<String> names = new HashSet<String>();
+		names.add("z");
+		assertFalse(variable.hasName(names));
+	}
+
+	@ParameterizedTest
+	@ValueSource(strings = { "x", "y" })
+	void equalsTest(String name) {
+		Variable variable1 = new Variable(5, name);
+		Variable variable2 = new Variable(5, name);
+		
+		assertEquals(variable1, variable2);
+	}
+
+	@ParameterizedTest
+	@ValueSource(strings = { "x", "y" })
+	void cloneTest(String name) {
+		Variable variable1 = new Variable(5, name);
+		Variable variable2 = variable1.clone();
+		
+		assertFalse(variable1 == variable2);
+		assertEquals(variable1, variable2);
+	}
+	
+	@ParameterizedTest
+	@ValueSource(strings = { "x", "y" })
+	public void toStringTest(String name) {
+		Variable variable = new Variable(5, name);
+		assertEquals("5.0" + name, variable.toString());
 	}
 
 }
